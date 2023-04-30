7cd9a764f7b28ad239d42860ddfb28ecdaac969f
LAETH08 <130323458+LAETH08@users.noreply.github.com>
2023-04-29 20:48:16 -0400

    Update final.php

--- final.php
+++ final.php
@@ -7,13 +7,30 @@ try {
     echo "Failed to connect to database: " . $e->getMessage();
 }
 
+$first_name = $_POST['first_name'] ?? '';
+$last_name = $_POST['last_name'] ?? '';
+$month = $_POST['month'] ?? '';
+$year = $_POST['year'] ?? '';
+
+$expenses = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
     $total_income = $_POST['total_income'];
     $expense_name = $_POST['expense_name'];
     $amount = $_POST['amount'];
     $date = $_POST['date'];
+
+    if (isset($_POST['delete'])) {
+        $id = $_POST['id'];
+
+        // Delete the expense from the database
+        $stmt = $pdo->prepare("DELETE FROM expenses WHERE id = ?");
+        if ($stmt->execute([$id])) {
+            echo "Expense deleted successfully!";
+        } else {
+            echo "Failed to delete expense.";
+        }
+    }
 
     // Calculate net income and total expenses
     $net_income = $total_income - $amount;
@@ -28,13 +45,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-$first_name = $_GET['first_name'] ?? '';
-$last_name = $_GET['last_name'] ?? '';
-$month = $_GET['month'] ?? '';
-$year = $_GET['year'] ?? '';
-
-$expenses = [];
-
 if ($first_name && $last_name) {
     $query = "SELECT * FROM expenses WHERE first_name = ? AND last_name = ?";
     $params = [$first_name, $last_name];
@@ -48,6 +58,12 @@ if ($first_name && $last_name) {
         $query .= " AND YEAR(date) = ?";
         $params[] = $year;
     }
+	$sort_column = $_POST['sort_column'] ?? '';
+	$sort_direction = $_POST['sort_direction'] ?? 'asc';
+
+if (!empty($sort_column)) {
+    $query .= " ORDER BY $sort_column $sort_direction";
+}
 
     $stmt = $pdo->prepare($query);
     if ($stmt->execute($params)) {
@@ -55,3 +71,76 @@ if ($first_name && $last_name) {
     }
 }
 ?>
+<head>
+<link rel="stylesheet" href="finalexam.css" type="text/css">
+</head>
+<body>
+    <h2>Results</h2>
+    <!-- Add the radio buttons for sorting --> 
+    <form action="final.php" method="POST">
+        <label for="month">Filter by Month:</label>
+        <select name="month" id="month">
+            <option value="">All Months</option>
+            <option value="1" <?= $month === '1' ? 'selected' : '' ?>>January</option>
+            <option value="2" <?= $month === '2' ? 'selected' : '' ?>>February</option>
+            <option value="3" <?= $month === '3' ? 'selected' : '' ?>>March</option>
+            <option value="4" <?= $month === '4' ? 'selected' : '' ?>>April</option>
+            <option value="5" <?= $month === '5' ? 'selected' : '' ?>>May</option>
+            <option value="6" <?= $month === '6' ? 'selected' : '' ?>>June</option>
+            <option value="7" <?= $month === '7' ? 'selected' : '' ?>>July</option>
+            <option value="8" <?= $month === '8' ? 'selected' : '' ?>>August</option>
+            <option value="9" <?= $month === '9' ? 'selected' : '' ?>>September</option>
+            <option value="10" <?= $month === '10' ? 'selected' : '' ?>>October</option>
+            <option value="11" <?= $month === '11' ? 'selected' : '' ?>>November</option>
+            <option value="12" <?= $month === '12' ? 'selected' : '' ?>>December</option>
+        </select>
+        <label for="year">Filter by Year:</label>
+        <select name="year" id="year">
+            <option value="">All Years</option>
+            <?php foreach ($years as $y): ?>
+                <option value="<?= $y ?>" <?= $year === $y ? 'selected' : '' ?>><?= $y ?></option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit">Apply Filters</button>
+    </form>
+	<?php if (!empty($expenses)): ?>
+    <table>
+        <tr>
+            <th>Expense Name</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($expenses as $expense): ?>
+            <tr>
+                <td><?= $expense['expense_name'] ?></td>
+                <td>$<?= $expense['amount'] ?></td>
+                <td><?= date('F j, Y', strtotime($expense['date'])) ?></td>
+                <td>
+                    <form action="final.php" method="POST">
+                        <input type="hidden" name="id" value="<?= $expense['id'] ?>">
+                        <button type="submit" name="delete">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+	<form action="final.php" method="POST">
+    <label for="sort_column">Sort By:</label>
+    <select name="sort_column" id="sort_column">
+        <option value="expense_name">Expense Name</option>
+        <option value="amount">Amount</option>
+        <option value="date">Date</option>
+    </select>
+    <label for="sort_direction">Direction:</label>
+    <input type="radio" name="sort_direction" value="asc" id="asc" checked>
+    <label for="asc">Ascending</label>
+    <input type="radio" name="sort_direction" value="desc" id="desc">
+    <label for="desc">Descending</label>
+    <button type="submit">Apply Filters</button>
+	</form>
+	<a href="final.html">Add Another Expense</a>
+	<?php else: ?>
+    <p>No expenses found.</p>
+</body>
+<?php endif; ?>
